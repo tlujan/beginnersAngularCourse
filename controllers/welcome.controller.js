d2193d18a4a94c0249616bcd18b571faa18d09c7
Josh R. Dunlavy <jdunlavy@editllc.com>
2017-01-18 00:11:30 -0800

    welcome screen changes (for convenience and not pertinent to what we covered in class)

--- controllers/welcome.controller.js
+++ controllers/welcome.controller.js
@@ -1,5 +1,29 @@
 (function (angular) {
 	angular
 		.module("application")
-		.controller("welcomeController", function () {	});
+		.controller("welcomeController", ["$route", function($route) {
+			this.simpleRoutes = linkToRoutes();
+
+			function linkToRoutes() {
+				let returnSimpleRoutes = [];
+
+				angular.forEach($route.routes, function(r) {
+					// we don't want redirect routes
+					if (!!r.redirectTo) {
+						return;
+					}
+
+					let simpleRoute = {
+						url: r.originalPath,
+						name: r.controller.replace(/controller/gi, "")
+					};
+
+					returnSimpleRoutes.push(simpleRoute);
+				});
+
+				console.log("This application has the following installed routes:", returnSimpleRoutes);
+
+				return returnSimpleRoutes;
+			}
+		}]);
 } (window.angular))
